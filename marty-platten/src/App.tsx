82775717d470dfe3a0274d1bb6a0cea4fda013bd
Justin Marty <justin.marty@icloud.com>
2025-05-17 16:04:42 +0200

    structure with react.js with vite and typescript

---
+++ marty-platten/src/App.tsx
@@ -0,0 +1,24 @@
+
+import logo from './assets/Logo_ohne Text.jpg'
+import './App.css'
+
+function App() {
+  return(
+    <>
+    <div>
+      <h1>Marty Plattenbeläge</h1>
+    </div>
+    <div>
+      <img src={logo} alt='image'></img>
+    </div>
+    <div>
+      <p>Telefon: 056 664 44 36</p>
+      <p>Email: marty-platten@bluewin.ch</p>
+      <p>Adresse: Obere Büelhalde 9, 5634 Merenschwand</p>
+    </div>
+    </>
+  )
+
+}
+
+export default App
